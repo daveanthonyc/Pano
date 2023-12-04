cc4a48913dd54c909e41375115636fd680d7c3e8
David Chiang <daveanthonyc@gmail.com>
2023-12-04 15:51:15 +1100

    added counter for practice

---
+++ src/components/Counter.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function Counter() {
+    const [count, setCount] = useState<number>(0)
+
+  return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count+1)}>count up</button>
+    </div>
+  )
+}
+
+export default Counter;

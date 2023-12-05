74d44f3b6dabb204b63abd82f6e8742191596b3a
David Chiang <daveanthonyc@gmail.com>
2023-12-05 14:59:44 +1100

    sidebar buttons

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,48 @@
-import { Box, Typography } from '@mui/material'
+import { Box, IconButton, Typography } from '@mui/material'
+import AssessmentIcon from '@mui/icons-material/Assessment';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import GridViewIcon from '@mui/icons-material/GridView';
+import WorkOutline from '@mui/icons-material/WorkOutline';
+
+type NavItem = {
+    text: string,
+    icon: any 
+}
+
+const navItems = [
+    {
+        text: "Dashboard",
+        icon: <GridViewIcon />,
+    },
+    {
+        text: "Analytics",
+        icon: <AssessmentIcon />,
+    },
+    {
+        text: "Projects",
+        icon: <WorkOutline />,
+    },
+    {
+        text: "All Issues",
+        icon: <TaskAltIcon />,
+    },
+]
 
 function Sidebar() {
   return (
     <Box>
         <Typography>Side bar</Typography>        
+        {
+            navItems.map((item: NavItem) => (
+                <Box display='flex' alignItems='center'>
+                    <IconButton color='body'>
+                        {item.icon}
+                    </IconButton>
+                    <Typography color='body' fontWeight='500'>{item.text}</Typography>
+                </Box>
+            ))
+        }
     </Box>
   )
 }
